5e3b72f485e460e088326a31e13a56dc2da2ddf7
hemantgamerrx-star <hemantgamerrx@gmail.com>
2026-04-17 13:43:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,25 @@
 
 clear
 
+# ===== BANNER =====
 echo "╔═══━━━── • ──━━━═══╗"
 echo "   HEMANT GAMERR"
 echo "╚═══━━━── • ──━━━═══╝"
 echo ""
 sleep 2
 
+# ===== UPDATE =====
 echo "⚙️ Updating system..."
 apt update -y
 
+# ===== PYTHON INSTALL =====
 echo "🐍 Installing Python..."
 apt install python3 python3-pip -y
 
-echo "📦 Installing modules..."
+# ===== MODULE INSTALL =====
+echo "📦 Installing Python modules..."
 pip3 install discord.py
 pip3 install qrcode pillow
 
 echo ""
-echo "✅ Setup Completed Successfully!"
+echo "✅ ALL DONE SUCCESSFULLY 🚀"
